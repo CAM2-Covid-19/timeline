b124a205315caf939b17264c8ed9fa1bc9cfe7d5
Akhil Chinnakotla <54074879+The-Indian-Chinna@users.noreply.github.com>
2020-06-10 08:48:58 -0400

    vishnu update

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,37 @@
+function httpGet(url){
+    var httpreq = new XMLHttpRequest();
+    httpreq.open("GET", url, false);
+    httpreq.send(null);
+    return JSON.parse(httpreq.responseText)
+}
 
 
-// disable if scroll snap is enabled
-// window.addEventListener("wheel", event => {
-//     //left = positive
-//     //up = positive
-    
-//     if (event.wheelDeltaX == 0 && event.wheelDeltaY >= -1 || event.wheelDeltaY <= -3){
-//         event.preventDefault()
-//         var mult = -0.50
+$(document).ready(function(){
+    //var worldwide = httpGet("https://corona-virus-stats.herokuapp.com/api/v1/cases/general-stats");
+    //var worldwide = httpGet("https://api.covid19api.com/summary");
+    // var countries = httpGet("https://corona-virus-stats.herokuapp.com/api/v1/cases/countries-search");
+    // console.log(worldwide)
+    // console.log(countries)
+    fetch("https://corona-virus-stats.herokuapp.com/api/v1/cases/general-stats").then(
+        response => response.json()
+    ).then(
+        data => {
+            // console.log(data);
+            console.log(data)
+            $("#wwlive").text(data["data"]["total_cases"])
+            $("#wwlived").text(data["data"]["death_cases"])
+        }
+    )
 
-//         var delta = mult * event.wheelDelta; 
-
-//         console.log('wheeldata', delta)
-//         console.log("xdelta", event.wheelDeltaX)
-//         console.log("ydelta", event.wheelDeltaY)
-//         console.log("details", event)
-
-
-//         var pst = $('.container').scrollLeft() + delta;
-//         $('.container').scrollLeft(pst);
-//     }
-// }, {passive: false});
+    fetch("https://corona-virus-stats.herokuapp.com/api/v1/cases/countries-search").then(
+        response => response.json()
+    ).then(
+        data => {
+            console.log(data["data"])
+            $("#usalive").text(data["data"]["rows"][1]["total_cases"])
+            $("#usalived").text(data["data"]["rows"][1]["total_deaths"])
+        }
+    )
+});
 
 
-// jQuery(function($) {
-//     $(".container").panelSnap();
-// });
