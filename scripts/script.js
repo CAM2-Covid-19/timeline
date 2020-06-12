5b7a14a8feaa157954cdb4d67846b4fef5b66820
Akhil Chinnakotla <54074879+The-Indian-Chinna@users.noreply.github.com>
2020-06-12 18:11:41 -0400

    hey

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,12 @@
-function httpGet(url){
-    var httpreq = new XMLHttpRequest();
-    httpreq.open("GET", url, false);
-    httpreq.send(null);
-    return JSON.parse(httpreq.responseText)
+// function httpGet(url){
+//     var httpreq = new XMLHttpRequest();
+//     httpreq.open("GET", url, false);
+//     httpreq.send(null);
+//     return JSON.parse(httpreq.responseText)
+// }
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 
@@ -12,26 +16,45 @@ $(document).ready(function(){
     // var countries = httpGet("https://corona-virus-stats.herokuapp.com/api/v1/cases/countries-search");
     // console.log(worldwide)
     // console.log(countries)
-    fetch("https://corona-virus-stats.herokuapp.com/api/v1/cases/general-stats").then(
-        response => response.json()
-    ).then(
-        data => {
-            // console.log(data);
-            console.log(data)
-            $("#wwlive").text(data["data"]["total_cases"])
-            $("#wwlived").text(data["data"]["death_cases"])
-        }
-    )
+    $.ajax({ 
+        type : "GET", 
+        url : "https://api.smartable.ai/coronavirus/stats/US", 
+        beforeSend: function(xhr){xhr.setRequestHeader('Subscription-Key', '3009d4ccc29e4808af1ccc25c69b4d5d');},
+        success : function(result) { 
+            //set your variable to the result 
+            console.log(result)
+            $("#usalive").text(numberWithCommas(result.stats.totalConfirmedCases));
+            $("#usalived").text(numberWithCommas(result.stats.totalDeaths));
+        }, 
+        error : function(result) { 
+          //handle the error 
+            console.log(result)
+        } 
+    });
+    $.ajax({ 
+        type : "GET", 
+        url : "https://api.smartable.ai/coronavirus/stats/global", 
+        beforeSend: function(xhr){xhr.setRequestHeader('Subscription-Key', '3009d4ccc29e4808af1ccc25c69b4d5d');},
+        success : function(result) { 
+            //set your variable to the result 
+            $("#wwlive").text(numberWithCommas(result.stats.totalConfirmedCases));
+            $("#wwlived").text(numberWithCommas(result.stats.totalDeaths));
+        }, 
+        error : function(result) { 
+          //handle the error 
+            console.log(result)
+        } 
+    });
 
-    fetch("https://corona-virus-stats.herokuapp.com/api/v1/cases/countries-search").then(
-        response => response.json()
-    ).then(
-        data => {
-            console.log(data["data"])
-            $("#usalive").text(data["data"]["rows"][1]["total_cases"])
-            $("#usalived").text(data["data"]["rows"][1]["total_deaths"])
-        }
-    )
+    // fetch("https://corona-virus-stats.herokuapp.com/api/v1/cases/countries-search").then(
+    //     response => response.json()
+    // ).then(
+    //     data => {
+    //         console.log(data["data"])
+    //         $("#usalive").text(data["data"]["rows"][1]["total_cases"])
+    //         $("#usalived").text(data["data"]["rows"][1]["total_deaths"])
+    //     }
+    // )
 });
 
 
